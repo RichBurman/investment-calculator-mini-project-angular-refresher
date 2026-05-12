ab73b1379ff623add884850bc542669458dd727a
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-05-12 12:18:47 +0100

    Refactor UserInputComponent to use signals for reactive state management and update onSubmit logic

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestmentInput } from '../investment-input.model';
 
@@ -12,18 +12,18 @@ import { InvestmentInput } from '../investment-input.model';
 export class UserInputComponent {
   calculate = output<InvestmentInput>();
 
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal ('5');
+  enteredDuration = signal ('10');
 
   onSubmit() {
     this.calculate.emit({
       // The "+" operator converts the string to a number
-      initialInvestment: +this.enteredInitialInvestment,
-      duration: +this.enteredDuration,
-      expectedReturn: + this.enteredExpectedReturn,
-      annualInvestment: +this.enteredAnnualInvestment
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: + this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment()
     });
   }
 }
