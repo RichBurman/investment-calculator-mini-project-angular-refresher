8b29637983508b8beac0acdc02233a25ad460d33
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-05-12 11:26:16 +0100

    Refactor investment data handling by introducing InvestmentInput model and updating related components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentInput } from './investment-input.model';
+
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,7 @@ import { UserInputComponent } from './user-input/user-input.component';
 })
 export class AppComponent {
 
-  onCalculateInvestmentResults(data: {
-    initialInvestment: number;
-    duration: number;
-    expectedReturn: number;
-    annualInvestment: number;
-  }) {
+  onCalculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } =
       data;
 
